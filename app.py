696064b8e02115cbd8b8054157fa8e6778be87ee
Ryan <ryan.verbaan@hotmail.com>
2022-03-08 20:46:51 +0100

    initial

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Congratulations, it's a web app!"
+@app.route('/')
+@app.route('/home')
+def home():
+    return "Congratulations"
 
 if __name__ == "__main__":
     #app.run(host="127.0.0.1", port=8080, debug=True)
+    app.debug = True
     app.run()
