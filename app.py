ae05b882d932452751d698d37d7a748cab9016b4
Ryan <ryan.verbaan@hotmail.com>
2022-03-10 18:13:31 +0100

    first version

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=["POST", "GET"])
+@app.route("/home", methods=["POST", "GET"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("index.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    app.run(host='127.0.0.1', port='5000', debug=True)
