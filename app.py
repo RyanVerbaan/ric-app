d2e3678339ac6b677706d8f27bf59e00efe507ae
Ryan <ryan.verbaan@hotmail.com>
2022-03-10 18:15:47 +0100

    first version

--- app.py
+++ app.py
@@ -15,4 +15,5 @@ def user(usr):
     return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port='5000', debug=True)
+    #app.run(host='127.0.0.1', port='5000', debug=True)
+    app.run(debug=True)
